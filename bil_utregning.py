ae9b1aa11062da1e976cf7af3d9222d5f9ce5c7b
Tina Polsangi Athari <tina.athari@gmail.com>
2024-11-06 11:27:18 +0100

    Improve structure by using separate classes

--- bil_utregning.py
+++ bil_utregning.py
@@ -1,58 +1,37 @@
-class Bil:
-    def __init__(self, ant_kjoerte_km:int, bil_type):
+class Elbil:
+    def __init__(self, ant_kjoerte_km:int):
         self._ant_kjoerte_km = ant_kjoerte_km
-        self._bil_type = bil_type
-        self._forsikring = 0
-        self._drivstoffbruk = 0
-        self._trafikkforsikringsavgift:float = 3058.7 #sum årlig
-        self._bomavgift = 0
-        self._total_kostnad = 0
-    
-    def forsikring(self):
-        if self._bil_type == "elbil":
-            self._forsikring = 5000
-            return self._forsikrings
-        elif self._bil_type == "bensinbil":
-            self._forsikring = 7500
-            return self._forsikring
-        else:
-            print("Feil input på type bil, skriv 'elbil' eller 'bensinbil'.")
-    
-    def drivstoffbruk(self):
-        if self._bil_type == "elbil":
-            self._ant_kjoerte_km * 2
-            return self._drivstoffbruk
-        elif self._bil_type == "bensinbil":
-            self._ant_kjoerte_km * 1
-            return self._ant_kjoerte_km
-        else:
-            print("Feil input på type bil, skriv 'elbil' eller 'bensinbil'.")
+        self._forsikring = 5000
+        self._trafikkforsikringsavgift = 8.38 * 365
+        self._drivstoffbruk = 0.2 * 2 * ant_kjoerte_km
+        self._bomavgift = 0.1 * ant_kjoerte_km
+        self._total_kostnad = self.hent_total_kostnad()
 
-    def hent_trafikkforsikringsavgift(self):
-        return self._trafikkforsikringsavgift
+    def hent_total_kostnad(self):
+        total_kostnad = self._forsikring + self._trafikkforsikringsavgift + self._drivstoffbruk + self._bomavgift
+        return total_kostnad
     
-    def hent_bomavgift(self):
-        if self._bil_type == "elbil":
-            self._bomavgift = self._ant_kjoerte_km * 0.1
-            return self._bomavgift
-        elif self._bil_type == "bensinbil":
-            self._bomavgift = self._ant_kjoerte_km * 0,3
-            return self._bomavgift
-        else:
-            print("Feil input på type bil, skriv 'elbil' eller 'bensinbil'.")
+    def __str__(self):
+        return f"Elbilen har kjørt: {self._ant_kjoerte_km} km.\nBilens årlige kostnader basert på dette er:\n\n{self._forsikring}kr i forsikring.\n{self._trafikkforsikringsavgift:.01f}kr i trafikkforsikringsavgift.\n{self._drivstoffbruk}kr i drivstoffbruk.\n{self._bomavgift}kr i bomavgift.\n\nBilens totalkostnader er: {self._total_kostnad}kr"
 
-    def hent_totalkostnad(self):
-        self._total_kostnad = (self._forsikring + self._drivstoffbruk + self._trafikkforsikringsavgift + self._bomavgift)
-        return self._total_kostnad
+class Bensinbil:
+    def __init__(self, ant_kjoerte_km):
+        self._ant_kjoerte_km = ant_kjoerte_km
+        self._forsikring = 7500
+        self._trafikkforsikringsavgift = 8.38 * 365
+        self._drivstoffbruk = 1 * ant_kjoerte_km
+        self._bomavgift = 0.3 * ant_kjoerte_km
+        self._total_kostnad = self.hent_total_kostnad()
+
+    def hent_total_kostnad(self):
+        total_kostnad = self._forsikring + self._trafikkforsikringsavgift + self._drivstoffbruk + self._bomavgift
+        return total_kostnad
 
     def __str__(self):
-        return(f"Bilen er av type: {self._bil_type} og har kjørt {self._ant_kjoerte_km} km.\n Bilen har total kostnad i et år på: {self._total_kostnad},-")
+        return f"Bensinbilen har kjørt: {self._ant_kjoerte_km} km.\nBilens årlige kostnader basert på dette er:\n\n{self._forsikring}kr i forsikring.\n{self._trafikkforsikringsavgift:.01f}kr i trafikkforsikringsavgift.\n{self._drivstoffbruk}kr i drivstoffbruk.\n{self._bomavgift}kr i bomavgift.\n\nBilens totalkostnader er: {self._total_kostnad}kr"
 
-bil_1 = Bil(10000, "elbil")
-bil_1.forsikring()
-bil_1.drivstoffbruk()
-bil_1.hent_trafikkforsikringsavgift()
-bil_1.hent_bomavgift()
-bil_1.hent_totalkostnad()
-
+bil_1 = Bensinbil(10000)
 print(bil_1)
+
+bil_2 = Elbil(10000)
+print(bil_2)
